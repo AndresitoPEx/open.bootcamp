690012c78e8a2ca74f2046cdb80a60417864f8ee
Jorge Romo <andresrt952@gmail.com>
2022-07-30 17:14:48 -0500

    herencia ok

--- herencia/herencia.java
+++ herencia/herencia.java
@@ -1,3 +1,6 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
 class HelloWorld {
     public static void main(String[] args) {
         Cliente cliente = new Cliente();
@@ -7,6 +10,8 @@ class HelloWorld {
         System.out.println(cliente.nombre);
         cliente.telefono = 123456789;
         System.out.println(cliente.telefono);
+        cliente.credito = 10000;
+        System.out.println(cliente.credito);
         
         Trabajador trabajador = new Trabajador();
         trabajador.edad = 23;
@@ -15,6 +20,9 @@ class HelloWorld {
         System.out.println(trabajador.nombre);
         trabajador.telefono = 987654321;
         System.out.println(trabajador.telefono);
+        trabajador.salario = 20000;
+        System.out.println(trabajador.salario);
+        
     }
 }
 
